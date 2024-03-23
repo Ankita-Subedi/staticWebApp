e113979dd96d9d60af213497c207020905feb172
Ankita <99787175+Ankitahub-tech@users.noreply.github.com>
2024-03-23 16:35:44 +0545

    new changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/text.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "motivational quote";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Gym website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is a gym website project made by me.",
+    url: "https://github.com/Ankita-Subedi/GymWebsite",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Tribute Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is a tribute website project made by me.",
+    url: "https://github.com/Ankita-Subedi/Tribute-website-using-HTMl-CSS",
   },
   {
-    title: "My Resume Site",
+    title: "Calculator project",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "This is a calculator project made by me",
+    url: "https://github.com/Ankita-Subedi/Calculator",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "This is my linkedin",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Hello this is my social handle, let's connect",
+    url: "https://www.linkedin.com/in/ankita-subedi-18a162232/",
   },
 ];
 
